9aeb695afd1e032f5a766f6ac13fc230e2c5a16e
er-arunkumarselvam <er.arunkumar_selvam@outlook.com>
2024-07-09 16:42:45 +0530

    scanner style changes

--- src/components/pages/Scanner.js
+++ src/components/pages/Scanner.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import ScannerElement from '../elements/ScannerElement'
+import icon from '../../assets/images/icon.svg';
 
 export default function Scanner() {
   return (
     <>
-    <h1>Scanner</h1>
-        <ScannerElement/>
+      <section className="container-fluid scanner">
+        <img src={icon} alt="icon" className='scanner__icon' />
+        <h1 className='scanner__title'>MTC-THAMBARAM</h1>
+        <p className='scanner__desc'>Click the button, scan the QR code, to access the form.</p>
+        <ScannerElement />
+      </section>
+        
     </>
   )
 }
