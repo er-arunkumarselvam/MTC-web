060d023423f028376a2826239bc6ff570c6e481a
er-arunkumarselvam <er.arunkumar_selvam@outlook.com>
2024-07-09 14:59:26 +0530

    changes

--- src/components/elements/ScannerElement.js
+++ src/components/elements/ScannerElement.js
@@ -1,66 +1,46 @@
 import React, { useRef, useEffect, useState } from 'react';
 import QrScanner from 'qr-scanner';
 
-export default function QRScanner() {
+export default function ScannerElement() {
     const videoRef = useRef(null);
     const [qrCodeResult, setQrCodeResult] = useState('');
-    const [error, setError] = useState('');
-    const [scanning, setScanning] = useState(false);
-    let qrScanner;
-
-    const startScanner = async () => {
-        if (videoRef.current) {
-            try {
-                qrScanner = new QrScanner(
-                    videoRef.current,
-                    result => {
-                        setQrCodeResult(result.data);
-                        setScanning(false);
-                        qrScanner.stop();
-                    },
-                    {
-                        onDecodeError: error => console.warn(error),
-                        highlightScanRegion: true,
-                        highlightCodeOutline: true
-                    }
-                );
-
-                const hasCamera = await QrScanner.hasCamera();
-                if (hasCamera) {
-                    qrScanner.start();
-                    setScanning(true);
-                } else {
-                    setError('No camera found.');
-                }
-            } catch (e) {
-                setError(`Error accessing the camera: ${e.message}`);
-            }
-        }
-    };
 
     useEffect(() => {
-        return () => {
-            if (qrScanner) {
+        let qrScanner;
+
+        if (videoRef.current) {
+            qrScanner = new QrScanner(
+                videoRef.current,
+                result => setQrCodeResult(result),
+                {
+                    onDecodeError: error => console.error(error),
+                    highlightScanRegion: true,
+                    highlightCodeOutline: true
+                }
+            );
+
+            // Start scanning when the component is mounted
+            qrScanner.start();
+
+            // Clean up and stop scanning when the component is unmounted
+            return () => {
                 qrScanner.stop();
                 qrScanner.destroy();
-            }
-        };
-    }, []);
+            };
+        }
+    }, [videoRef]);
 
     const handleScanButtonClick = () => {
-        if (!scanning) {
-            setQrCodeResult('');
-            setError('');
-            startScanner();
-        }
+        // if (videoRef.current) {
+        //     videoRef.current.classList.toggle('hidden');  // Show or hide the video element
+        // }
     };
 
     return (
-        <div>
+        <>
             <button onClick={handleScanButtonClick}>Scan QR Code</button>
-            <video ref={videoRef} style={{ width: '100%', display: scanning ? 'block' : 'none' }}></video>
+            <video ref={videoRef}  style={{ width: '100%' }}></video>
             {qrCodeResult && <p>QR Code Result: {qrCodeResult}</p>}
-            {error && <p className="error">{error}</p>}
-        </div>
+        </>
     );
 }
